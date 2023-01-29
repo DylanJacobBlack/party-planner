fdc7cf4fb3aef4059ae4c23b59add38701f37820
Dylan <dylan@Dylans-Air.attlocal.net>
2023-01-28 20:23:06 -0600

    created basic prototype of app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { useQuery } from 'react-query';
+import { Spinner } from 'react-bootstrap';
 
-function App() {
+interface PartyItem {
+  name: string;
+  link: string;
+}
+
+interface PartyTheme {
+  theme: string;
+  items: PartyItem[];
+}
+
+
+const ChatGPTQuery = () => {
+  const [theme, setTheme] = useState('');
+  const [partyData, setPartyData] = useState<PartyTheme>({ theme: '', items: [] });
+
+  const { isLoading, error, data } = useQuery<object, Error, object>(
+    theme,
+    async (key) => {
+      // Make a request to the OpenAI API to query ChatGPT with the party theme
+      const response = await fetch(`https://api.openai.com/v1/engines/davinci-codex/completions`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer YOUR_API_KEY'
+        },
+        body: JSON.stringify({
+          prompt: `What are the top 3 items available on Amazon for throwing a ${theme} party?`,
+          max_tokens: 100
+        })
+      });
+
+      // Get the items from the response
+      const json = await response.json();
+      const items = json.choices[0].text.split("\n").filter((i: string) => i.startsWith("- ") && i.includes("http"));
+      const partyItems = items.map((i: string) => {
+        const link = i.substring(i.indexOf("http"));
+        const name = i.substring(2, i.indexOf("http") - 1);
+        return { name, link };
+      });
+      setPartyData({ theme: key.toString(), items: partyItems });
+    },
+    {
+      enabled: theme.length > 0,
+      onSuccess: () => setTheme('')
+    }
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form>
+        <input
+          type="text"
+          placeholder="Enter party theme"
+          value={theme}
+          onChange={e => setTheme(e.target.value)}
+        />
+        <button type="submit" onClick={() => setTheme(theme)}>
+          Search
+        </button>
+        <button type="submit" onClick={() => setTheme('')}>
+          Clear
+        </button>
+      </form>
+      {isLoading ? (
+        <Spinner animation="border" role="status">
+          <span className="sr-only">Loading...</span>
+        </Spinner>
+      ) : error ? (
+        <div>Error: {error.message}</div>
+      ) : data ? (
+        <>
+          <h3>{partyData.theme} Party</h3>
+          <ul>
+            {partyData.items.map((item, index) => (
+              <li key={index}>
+
+
+                <a href={item.link}>{item.name}</a>
+              </li>
+            ))}
+          </ul>
+        </>
+      ) : (
+        <div>Please enter a party theme</div>
+      )}
     </div>
   );
 }
 
-export default App;
+export default ChatGPTQuery;
